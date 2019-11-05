c26ec37bd9aac06b805e2afee966f43e7976ba41
stefanopaulo <stefanopaulo47@gmail.com>
2019-11-05 19:48:28 -0300

    curso de html5

---
+++ exercicios/form/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/usuarios', (req, res) => {
+    console.log(req.body)
+    res.send('<h1>Parabéns. Usuário incluido!</h1>')
+})
+
+app.post('/usuarios/:id', (req, res) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    res.send('<h1>Parabéns. Usuário alterado!</h1>')
+})
+
+app.listen(3003)
